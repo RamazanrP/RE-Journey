ecb6a2e55778177fd8aba8ea8249793006c46fa5
RamazanrP <186416588+RamazanrP@users.noreply.github.com>
2026-07-27 18:26:16 +0300

    Update detectors.py

--- RE-Navigator/analyzer/detectors.py
+++ RE-Navigator/analyzer/detectors.py
@@ -15,3 +15,62 @@ def detect_auth_logic(func):
         reasons.append("High number of conditional branches → decision-heavy logic")
 
     return score, reasons
+
+def detect_memory_risk(func):
+    score = 0
+    reasons = []
+
+    dangerous = ["strcpy", "gets", "scanf", "memcpy", "strcat"]
+
+    for call in func["calls"]:
+        if call in dangerous:
+            score += 3
+            reasons.append(f"Uses {call} → potential memory vulnerability")
+
+    return score, reasons
+
+def detect_centrality(func):
+    score = 0
+    reasons = []
+
+    if func["xrefs"] > 5:
+        score += 4
+        reasons.append("High number of cross-references → central function") # Daha çok çağrılıyorsa daha kritik de olabilir Junk Code da olabilir. Umuyoruz ki core func. çıksın :)
+
+    if len(func["callers"]) > 3:
+        score += 2
+        reasons.append("Called from multiple locations → shared logic")
+
+    return score, reasons
+
+def detect_complexity(func):
+    score = 0
+    reasons = []
+    # Yine aynı şekilde umuyoruz ki Core Logic sebebiyle kompleks bir fonksiyon yazışmıştır
+    if func["loops"]:
+        score += 2
+        reasons.append("Contains loops → possible data processing")
+
+    if func["conditions"] > 8:
+        score += 3
+        reasons.append("High branching → complex logic")
+
+    if func["size"] > 150:
+        score += 2
+        reasons.append("Large function → likely core logic")
+
+    return score, reasons
+
+def detect_sensitive_apis(func):
+    score = 0
+    reasons = []
+
+    sensitive = ["CreateFile", "ReadFile", "WriteFile","send", "recv","Crypt", "AES", "SHA"]
+
+    for imp in func["imports"]:
+        for s in sensitive:
+            if s.lower() in imp.lower():
+                score += 3
+                reasons.append(f"Uses sensitive API '{imp}'")
+
+    return score, reasons
