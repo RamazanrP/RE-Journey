de03efe4bcb7692d4d6023127ffed3f7648c4e48
RamazanrP <186416588+RamazanrP@users.noreply.github.com>
2026-07-27 18:56:48 +0300

    Update main.py

--- RE-Navigator/main.py
+++ RE-Navigator/main.py
@@ -1,6 +1,5 @@
 from analyzer.language_detector import detect_language
-from analyzer.c_analyzer import analyze_c
-from analyzer.cpp_analyzer import analyze_cpp
+from analyzer.core import analyze_function
 from analyzer.classifier import classify
 from utils.color import colorize
 import json
@@ -15,12 +14,8 @@ def main():
     lang = detect_language(functions)
     print(f"\n[+] Detected Language: {lang}\n")
 
-    if lang == "C++":
-        results = analyze_cpp(functions)
-    else:
-        results = analyze_c(functions)
-
-    for func, score, reasons in results:
+    for func in functions:
+        score, reasons = analyze_function(func, lang)
         level = classify(score)
 
         print(colorize(level, f"[{level}] {func['name']} (score: {score})"))
