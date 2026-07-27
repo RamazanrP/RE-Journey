f451114653a3e70d698f756f19e586176d321c49
RamazanrP <186416588+RamazanrP@users.noreply.github.com>
2026-07-27 19:32:25 +0300

    Update main.py

--- RE-Navigator/main.py
+++ RE-Navigator/main.py
@@ -2,11 +2,9 @@ from analyzer.language_detector import detect_language
 from analyzer.core import analyze_function
 from analyzer.classifier import classify
 from utils.color import colorize
+from analyzer.detectors import detect_weak_signals
+from analyzer.callgraph import build_call_map, find_entry, dfs_paths
 import json
-
-def load_data():
-    with open("data.json") as f:
-        return json.load(f)
 
 def main():
     functions = load_data()
@@ -22,6 +20,23 @@ def main():
 
         for r in reasons:
             print(f"   → {r}")
+
+        if level in ["HIGH", "MEDIUM"]: # Spam gibi gözükmemesi için sadeec HİGH ve Medium'da notlar gösterilsin istedim
+            notes = detect_weak_signals(func)
+
+            if notes:
+                print("   [Notes]")
+                for n in notes:
+                    print(f"      - {n}")
+
+            print("\n[+] Call Chains:\n")
+            call_map = build_call_map(functions)
+            entry = find_entry(functions)
+
+            chains = dfs_paths(call_map, entry)
+
+            for chain in chains:
+                print(" → ".join(chain))
 
         print()
 
